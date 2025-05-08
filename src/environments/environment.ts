5b954edee0a8fd8c81de00081157f14de878c167
Ottoniel Molina <ottonielmolina@gmail.com>
2025-05-07 20:05:19 -0600

    core(environment): genera archivos environment.

---
+++ src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    urlPlaces: 'https://nominatim.openstreetmap.org/search?format=json&q={place}',
+    urlDetectIp: 'https://ipv4.myexternalip.com/json',
+    urlCountryIpAddress: 'https://api.country.is/',
+};
