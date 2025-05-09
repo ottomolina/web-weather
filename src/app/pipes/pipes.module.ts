f42e1167715e278bd5e36c4c8c6857f8a3aba245
Ottoniel Molina <ottonielmolina@gmail.com>
2025-05-08 23:00:51 -0600

    feat(format-number): agrega pipe para formateo de números.

--- src/app/pipes/pipes.module.ts
+++ src/app/pipes/pipes.module.ts
@@ -2,17 +2,20 @@ import { NgModule } from '@angular/core';
 import { DayOfWeek } from './day-of-week.pipe';
 import { WeatherCodePipe } from './weather-code.pipe';
 import { WeatherCodeIconPipe } from './weather-code-icon.pipe';
+import { FormatNumberPipe } from './format-number.pipe';
 
 @NgModule({
     declarations: [
         DayOfWeek,
         WeatherCodePipe,
-        WeatherCodeIconPipe
+        WeatherCodeIconPipe,
+        FormatNumberPipe
     ],
     exports: [
         DayOfWeek,
         WeatherCodePipe,
-        WeatherCodeIconPipe
+        WeatherCodeIconPipe,
+        FormatNumberPipe
     ],
 })
 
