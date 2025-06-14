35478e9844c96db97c81a083f37b74a505b6ab35
Ottoniel Molina <ottonielmolina@gmail.com>
2025-06-13 22:23:47 -0600

    fix(day-of-week.pipe): amplífica pipe day-of-week para obtener nombre del día para otras funcionalidades.

--- src/app/pipes/day-of-week.pipe.ts
+++ src/app/pipes/day-of-week.pipe.ts
@@ -7,12 +7,35 @@ const nameDays = ['Domingo','Lunes','Martes','Miércoles','Jueves','Viernes','S�
 })
 export class DayOfWeek implements PipeTransform {
 
-    transform(time?: string) {
+    transform(time?: string, card?: boolean) {
+
+        const date = this.transformIntoDate(time);
+        if(card) {
+            if(this.compareDates(date, this.transformIntoDate())) {
+                return 'Hoy';
+            }
+            if(this.compareDates(date, this.transformIntoDate(undefined, 1))) {
+                return 'Mañana';
+            }
+        }
+        return nameDays[date.getDay()];
+    }
+
+    transformIntoDate(time?: string, addDay: number = 0) {
         const date = new Date(
             (time ? new Date(`${time}T00:00:00`) : new Date())
             .toLocaleString('en-US', { timeZone: Intl.DateTimeFormat().resolvedOptions().timeZone })
         );
-        return nameDays[date.getDay()];
+        date.setDate(date.getDate()+addDay)
+        return date;
+    }
+
+    compareDates(date1: Date, date2: Date) {
+        return (
+            date1.getDate() == date2.getDate() &&
+            date1.getMonth() == date2.getMonth() &&
+            date1.getFullYear() == date2.getFullYear()
+        )
     }
 
 }
