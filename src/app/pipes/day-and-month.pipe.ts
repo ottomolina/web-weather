3232d92c198130a84334e3a311cd558d38f3ab0c
Ottoniel Molina <ottonielmolina@gmail.com>
2025-06-18 20:57:20 -0600

    feat(day-and-month.pipe): modifica pipe day and month.
    Modificación en pipe day and month para devolver dos tipos de texto dependiendo del parámetro formatMonth.

--- src/app/pipes/day-and-month.pipe.ts
+++ src/app/pipes/day-and-month.pipe.ts
@@ -5,13 +5,13 @@ import { Pipe, PipeTransform } from "@angular/core";
 })
 export class DayAndMonth implements PipeTransform {
 
-    transform(time: string) {
+    transform(time: string, formatMonth: 'short'|'long'='short') {
         const date = new Date(new Date(`${time}T00:00:00`)
             .toLocaleString('en-US', { timeZone: Intl.DateTimeFormat().resolvedOptions().timeZone })
         );
         const day = date.getDate();
-        const month = date.toLocaleString('default', { month: 'short' });
-        return `${day} ${month}`;
+        const month = date.toLocaleString('default', { month: formatMonth });
+        return `${day}${formatMonth === 'short' ? ' ':' de '}${month}`;
     }
 
 }
