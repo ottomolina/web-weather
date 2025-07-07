06093f513670d2703b98c0f57f618da9860a0520
Ottoniel Molina <ottonielmolina@gmail.com>
2025-07-06 22:49:00 -0600

    feat(translate): agrega traducción para título de página y spinner de carga.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { ToastService } from './components/toast/toast.service';
+import { Title } from '@angular/platform-browser';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
@@ -10,5 +12,11 @@ export class AppComponent {
   
   constructor(
     public toastService: ToastService,
-  ) { }
+    private translate: TranslateService,
+    private title: Title,
+  ) {
+    setTimeout(() => {
+      this.title.setTitle(this.translate.instant('app.title'));
+    }, 100);
+  }
 }
